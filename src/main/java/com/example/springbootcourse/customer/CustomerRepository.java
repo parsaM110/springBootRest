4c31da186b308282550c0c68e291db667902ef09
parsaM110 <pmomeni80@gmail.com>
2023-10-06 15:30:35 +0330

    adding singletonList instead of Arrays.asList : useless:14-3

--- src/main/java/com/example/springbootcourse/customer/CustomerRepository.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.example.springbootcourse.customer;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +13,12 @@ public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers(){
         //Todo connect to real DB
-        return Collections.emptyList();
+//        return Arrays.asList(
+//                new Customer(1L, " Todo. Implement DB")
+//        );
+        return Collections.singletonList(
+                new Customer(1L, " Todo. Implement DB")
+        );
+        // no difference
     }
 }
