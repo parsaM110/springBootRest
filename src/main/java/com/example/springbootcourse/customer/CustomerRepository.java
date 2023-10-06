4dd65d17d90d1dce3a0a1fa78a96bc58695693ca
parsaM110 <pmomeni80@gmail.com>
2023-10-06 15:22:56 +0330

    make CustomerRepository primary in case of not passing qualifier it will automatically be taken :14-2

--- src/main/java/com/example/springbootcourse/customer/CustomerRepository.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerRepository.java
@@ -1,11 +1,13 @@
 package com.example.springbootcourse.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
 @Component
+@Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers(){
