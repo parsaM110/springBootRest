6cca252e2a643628a9a35f34de18e0e61bd256df
parsaM110 <pmomeni80@gmail.com>
2023-10-06 22:54:53 +0330

    define new v2 controller api and deprecate the v1 version:25-2

--- src/main/java/com/example/springbootcourse/customer/CustomerControllerV2.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerControllerV2.java
@@ -3,23 +3,23 @@ package com.example.springbootcourse.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
-@RequestMapping(path = "api/v1/customer")
+@RequestMapping(path = "api/v2/customer")
 @RestController
-@Deprecated
-public class CustomerController {
+public class CustomerControllerV2 {
 
     private final CustomerService customerService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerControllerV2(CustomerService customerService) {
         this.customerService = customerService;
     }
 
     @GetMapping(value = "all")
     List<Customer> getCustomer() {
-        return customerService.getCustomer();
+        return List.of(new Customer(0L, "v2", "v2"));
     }
 
     @PostMapping //we map request json body into customer obj
