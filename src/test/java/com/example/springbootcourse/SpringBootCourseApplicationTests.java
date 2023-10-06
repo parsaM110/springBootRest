9780150d6f9a893183349d7ace38864a010fce20
parsaM110 <pmomeni80@gmail.com>
2023-10-06 12:48:13 +0330

    "initial commit"

---
+++ src/test/java/com/example/springbootcourse/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
