adc82d6acb9866eb0fad838d9b9de98bdb571d91
parsaM110 <pmomeni80@gmail.com>
2023-10-06 16:16:39 +0330

    using Service and Repos instead 0f Component:15

--- src/main/java/com/example/springbootcourse/customer/CustomerRepository.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerRepository.java
@@ -2,12 +2,13 @@ package com.example.springbootcourse.customer;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Component
+@Repository
 @Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
