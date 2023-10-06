5ae505016dcf6619dada3099a4ebc536e7a6a120
parsaM110 <pmomeni80@gmail.com>
2023-10-06 13:39:31 +0330

    create bean in Service by @component which is Singelton and you can inject it and using Authwired :12

--- src/main/java/com/example/springbootcourse/customer/CustomerController.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerController.java
@@ -1,6 +1,7 @@
 package com.example.springbootcourse.customer;
 
 import com.example.springbootcourse.SpringBootCourseApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,9 @@ public class CustomerController {
 
     private final CustomerService customerService ;
 
-    public CustomerController(){
-        customerService = new CustomerService();
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService =  customerService;
     }
 
     @GetMapping
--- src/main/java/com/example/springbootcourse/customer/CustomerService.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerService.java
@@ -1,5 +1,9 @@
 package com.example.springbootcourse.customer;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
     Customer getCustomer(){
         return new Customer(1L,"James Bond");
