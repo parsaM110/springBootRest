ce26e106c5efc8cca2e1c4abe79c8b9639b2eab0
parsaM110 <pmomeni80@gmail.com>
2023-10-07 11:35:02 +0330

    add exception handler to controller get endpoint with some modification :36

--- src/main/java/com/example/springbootcourse/customer/CustomerControllerV2.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerControllerV2.java
@@ -1,5 +1,6 @@
 package com.example.springbootcourse.customer;
 
+import com.example.springbootcourse.exception.ApiRequestException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +28,13 @@ public class CustomerControllerV2 {
     @GetMapping(path = "{customerId}")
     Customer getCustomer(@PathVariable("customerId") Long id)  {
         return customerService.getCustomer(id);
+    }
+
+    @GetMapping(path = "{customerId}/exception")
+    Customer getCustomerException(@PathVariable("customerId") Long id)  {
+        throw new ApiRequestException(
+                "ApiRequestException for customer " + id
+        );
     }
 
     @GetMapping()
--- src/main/java/com/example/springbootcourse/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/springbootcourse/exception/ApiExceptionHandler.java
@@ -2,12 +2,15 @@ package com.example.springbootcourse.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZonedDateTime;
 
-public class ExceptionHandler {
+@ControllerAdvice
+public class ApiExceptionHandler {
 
-
+    @ExceptionHandler(value = {ApiRequestException.class})
     public ResponseEntity<Object> handleApiRequestException(
             ApiRequestException e
     ){
