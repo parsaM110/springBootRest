5d686cc7f74891b027c00c16c4f585dec6c47055
parsaM110 <pmomeni80@gmail.com>
2023-10-07 23:34:10 +0330

    accessing application.properties through code  :56

--- src/main/java/com/example/springbootcourse/customer/CustomerConfiguration.java
+++ src/main/java/com/example/springbootcourse/customer/CustomerConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.springbootcourse.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
@@ -11,12 +13,21 @@ public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
     @Bean
         // if you dont put this shit it wont work
     CommandLineRunner commandLineRunner() {
         return args -> {
 
             System.out.println("\u001B[34m" + "Command line runner hooray" + "\u001B[0m");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.company.name"));
+
 
         };
     }
