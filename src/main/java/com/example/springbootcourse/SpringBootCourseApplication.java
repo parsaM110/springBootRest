16da28b5cc22c04d37dd767cf2e83cef2515e23f
parsaM110 <pmomeni80@gmail.com>
2023-10-07 20:03:58 +0330

    adding spring cloud - openFeign which is like glorified OkHTTP :44

--- src/main/java/com/example/springbootcourse/SpringBootCourseApplication.java
+++ src/main/java/com/example/springbootcourse/SpringBootCourseApplication.java
@@ -2,10 +2,11 @@ package com.example.springbootcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringBootCourseApplication {
 
     public static void main(String[] args) {
